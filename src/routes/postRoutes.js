9dfae26704e14c1979079bf9e188c5543d29dd3a
Kayo Elias <46985165+OyakSaile@users.noreply.github.com>
2022-06-26 18:04:03 -0300

    project structure

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,54 +1,13 @@
 const express = require('express');
 const router = express.Router()
 const Post = require('../models/Post')
+const controller = require('../controllers/postController')
 
+router.get('/', controller.get)
 
-router.get('/', (req, res) => {
-  Post.findAll({}).then((data) => {
-    res.send(data)
-  }).catch(err => {
-    console.error(err)
-  })
-})
+router.delete("/:id", controller.delete)
 
-router.delete('/:id', async (req, res) => {
-
-  const project = await Post.findByPk(req.params.id);
-  if (project === null) {
-    res.send('Não foi encontrado este ID')
-  } else {
-    try {
-      const response = await Post.destroy({
-        where: {
-          id: req.params.id
-        }
-      })
-
-      if (response) {
-        res.send('deletado')
-      }
-    }
-    catch (err) {
-      console.log(err)
-      res.send("erro")
-    }
-  }
-})
-
-
-router.post('/add', (req, res) => {
-  let { post_title, post_creator, post_content } = req.body
-  Post.create({
-    post_title,
-    post_creator,
-    post_content
-  }).then((data) => {
-    res.send(data)
-  })
-    .catch(err => {
-      console.error(err)
-    })
-})
+router.post('/add', controller.post)
 
 
 module.exports = router
