9ea2d738c26b4c6e3f80da7271f3457835d83dec
Kayo Elias <46985165+OyakSaile@users.noreply.github.com>
2022-06-26 18:51:15 -0300

    put routes

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,6 +1,6 @@
 const Post = require('../models/Post')
 
-exports.get =((req, res) => {
+exports.get = (async (req, res) => {
   Post.findAll({}).then((data) => {
     res.send(data)
   }).catch(err => {
@@ -8,17 +8,34 @@ exports.get =((req, res) => {
   })
 })
 
-exports.delete =(async (req, res) => {
-  const project = await Post.findByPk(req.params.id);
-  if (project === null) {
+exports.put = (async (req, res) => {
+  const { post_title, post_content, post_creator, id } = req.body
+
+  try {
+    const response = await Post.findByPk(id)
+    response.post_creator = post_creator
+    response.post_title = post_title
+    response.post_content = post_content
+    console.log(response)
+    const save = await response.save()
+    console.log(save)
+
+    res.send('Finalizado e editado')
+  }
+
+  catch (err) {
+    console.log(err)
+    res.send('Erro')
+  }
+})
+
+exports.delete = (async (req, res) => {
+  const post = await Post.findByPk(req.params.id);
+  if (post === null) {
     res.send('Não foi encontrado este ID')
   } else {
     try {
-      const response = await Post.destroy({
-        where: {
-          id: req.params.id
-        }
-      })
+      const response = await post.destroy()
 
       if (response) {
         res.send('deletado')
@@ -31,17 +48,19 @@ exports.delete =(async (req, res) => {
   }
 })
 
-
-exports.post = ((req, res) => {
+exports.post = (async (req, res) => {
   let { post_title, post_creator, post_content } = req.body
-  Post.create({
-    post_title,
-    post_creator,
-    post_content
-  }).then((data) => {
-    res.send(data)
-  })
-    .catch(err => {
-      console.error(err)
+  try {
+    const response = await Post.create({
+      post_title,
+      post_creator,
+      post_content
     })
+    if (response) {
+      res.send('Data')
+    }
+  } catch (err) {
+    res.send('Ocorreu um erro.')
+  }
+
 })
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router()
-const Post = require('../models/Post')
 const controller = require('../controllers/postController')
 
 router.get('/', controller.get)
 
 router.delete("/:id", controller.delete)
-
+router.put("/edit", controller.put)
 router.post('/add', controller.post)
 
 
